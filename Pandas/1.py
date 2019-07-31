bbb404a414e2afbc6c1ed5b1a29413f9382e32fe
greg008 <greg008@poczta.fm>
2019-07-31 09:51:53 +0200

    pandas full lesson

--- Pandas/1.py
+++ Pandas/1.py
@@ -1,4 +1,5 @@
 import pandas
+import matplotlib as plt
 
 df = pandas.read_csv('data-geo.csv', sep=',', header=0)
 # print(df)
@@ -10,9 +11,44 @@ print(df.shape)
 # print(df['City'])
 
 # 2 kolumny
-print(df[['City', 'State']])
+# print(df[['City', 'State']])
 
 # konkretne zalozenia
 
-print(df[df['State'] == 'Texas'])
+# print(df[df['State'] == 'Texas'])
+
+# numer wiersza
+# print(df.iloc[120])
+
+# sortowanie wd kolumny
+# print(df.sort_values(by=['2015 median sales price'], ascending=False))
+
+# modyfikowanie data frame'a
+# df['newcol'] = 'test'
+# print(df)
+
+# modyfikowanie df z warunkiem
+def addColumn(row):
+    if row['2015 median sales price'] > 200:
+        return 'high'
+    else:
+        return 'high'
+
+# apply stasuje do kazdego wiersza a axis 1 oznacza ze do kolumn czyli osi pionowej
+
+# df['isHigh'] = df.apply(addColumn, axis=1)
+# print(df)
+
+# group_by po jakich polach ma byc grupowany, zgrupowało po sredniej tam gdzie były liczby
+# poczytaj o funkcjach agregujących
+
+dfnew =  df.groupby(['State']).mean()
+
+
+# rysowanie wykresów normalny i slupkowy
+dfnew.plot()
+plt.pyplot.show()
+dfnew['2015 median sales price'].plot(kind='bar')
+plt.pyplot.show()
+
 
